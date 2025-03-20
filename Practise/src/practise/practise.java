3c629ac1817bb0d976e1097b8f299063825537b4
Sushant <gurun@134.154.73.222>
2025-03-20 14:05:22 -0700

    Pushing

---
+++ Practise/src/practise/practise.java
@@ -0,0 +1,11 @@
+package practise;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class practise{
+	public static void main(String[] args) {
+		//tell me where are you looking 
+		System.out.println("Wasssssssup trying to figure out how to use git");
+	}
+}
