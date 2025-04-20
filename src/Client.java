b97e5e8c761206d7fd92b1b16b80ee359ab59891
elle-zej <j17overstreet@gmail.com>
2025-04-20 01:25:15 -0700

    created and started client, conversation, and server classes

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,17 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Client {
+	public static void main(String[] args) throws IOException{
+		 
+		// change place holders below:
+		String host = "1";
+		int port = 1;
+		
+		Socket socket = new Socket(host, port);
+	}
 
 }
--- src/Conversation.java
+++ src/Conversation.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Conversation {
+	private List<Message> conversationHistory;
+	private List<User> members;
+	private int conversationID;
+	private static int IDCount = 0;
+	
+	Conversation(List<Message> conversationHistory, List<User> members){
+		this.conversationHistory = conversationHistory;
+		this.members = members;
+		this.conversationID = IDCount++;
+	}
+	
+	public void addMessage(Message message) {
+		conversationHistory.add(message);
+	}
 
 }
