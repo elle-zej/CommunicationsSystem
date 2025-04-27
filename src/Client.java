038852788b4a1fd5029545038e6c515b72720f65
elle-zej <j17overstreet@gmail.com>
2025-04-27 15:28:59 -0700

    revised Client and Conversation classes: onViewConversations and helpers

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -59,8 +58,8 @@ public class Client {
             	sc.nextLine();
             	
             	switch(choice) {
-            	case 1: onSendMessage(completeUser, out, in); break;
-            	case 2: doViewConversations(completeUser); break;
+            	case 1: onSendMessage(completeUser,sc, out, in); break;
+            	case 2: onViewConversations(completeUser, sc, out, in); break;
             	}
             	
             	
@@ -79,27 +78,22 @@ public class Client {
 	        	sc.nextLine();
 	        	
 	        	switch(choice) {
-	        	case 1: onSendMessage(completeUser, out, in); break;
-	        	case 2: doViewConversations(completeUser); break;
+	        	case 1: onSendMessage(completeUser, sc, out, in); break;
+	        	case 2: onViewConversations(completeUser, sc, out, in); break;
 				}
 			}
 		}
-        
-        
-        
 
-	
 	}
 	
-	private static void onSendMessage(User user, ObjectOutputStream out, ObjectInputStream in)
+	private static void onSendMessage(User user, Scanner sc, ObjectOutputStream out, ObjectInputStream in)
 			throws IOException, ClassNotFoundException {
-		Scanner sc = new Scanner(System.in);
 		List<String> members = new ArrayList<String>();
 		
 		System.out.println("Enter recipients separated by commas: ");
 		//ASCII?
 		String input = sc.nextLine();
-		input.toUpperCase();
+		input = input.toUpperCase();
 		//handle incorrect input
 		String[] recipients = input.split(",");
 		//create list of recipients
@@ -109,33 +103,105 @@ public class Client {
 		
 		System.out.println("Enter message: ");
 		String msg = sc.nextLine();
-		
+		//send message to server
 		Message message = new Message(user, members, msg, Status.request);
 		out.writeObject(message);
 		out.flush();
-		
+		//server sends message confirmation
 		Message serverMessage = (Message) in.readObject();
 		//GUI pop-up
 		if(serverMessage.getStatus().equals(Status.fail)){
 			System.out.println(serverMessage.getContent());
 		}
-		
-		
 	}
 
-	private static void doViewConversations(User user) {}
+	private static void onViewConversations(User user, Scanner sc, ObjectOutputStream out, ObjectInputStream in) 
+			throws IOException, ClassNotFoundException {
+		//Send request to viewConversations
+		Message viewConversationsRequest = new Message(user, "viewConversationsRequest", Status.request);
+		out.writeObject(viewConversationsRequest);
+		out.flush();
+		//server sends message confirmation
+		Message serverMessage = (Message) in.readObject();
+		if(serverMessage.getStatus().equals(Status.fail)) {
+			System.out.println(serverMessage.getContent());
+		}
+		//take in all conversations
+		@SuppressWarnings("unchecked")
+		List<Conversation> conversations = (List<Conversation>) in.readObject();
+		
+		System.out.println("Chats: ");
+		for(int i =0; i < conversations.size(); i++) {
+			System.out.println(i + conversations.get(i).getMembersString());
+		}
+		
+		int conversationChoice = sc.nextInt();
+		
+		//print all elements of the conversation
+		System.out.println("Conversation " + conversations.get(conversationChoice).getConversationIDString() + "\n");
+		System.out.println(conversations.get(conversationChoice).getMessagesString() + "\n\n");
+		System.out.println("Enter: \n "
+				+ "1. to send message \n"
+				+ "2. return to main menu");
+		//check user input
+		int choice;
+		while(true) {
+			if (sc.hasNextInt()) {
+				choice = sc.nextInt();
+				//consume next line character
+				sc.nextLine();
+				if (choice == 1 || choice ==2) {
+					break;
+				}
+				else {
+					System.out.println("Error: Enter a number 1 or 2: ");
+				}
+			}
+			else {
+				System.out.println("Error: Enter a number 1 or 2: ");
+			}
+		}
+		
+		while(true) {
+		if(choice == 1) {
+			System.out.println("Enter message: ");
+			String msg = sc.nextLine().trim();
+			List<String> receivers = conversations.get(conversationChoice).getRecipients(user);
+			Message message = new Message(user, receivers, msg, Status.request);
+			out.writeObject(message);
+			out.flush();
+			//server sends message confirmation
+			serverMessage = (Message) in.readObject();
+			//GUI pop-up
+			if(serverMessage.getStatus().equals(Status.fail)){
+				System.out.println(serverMessage.getContent());
+			}
+			}
+		if(choice == 2){return;}
+		else {
+			System.out.println("Enter 1 or 2");
+			choice = sc.nextInt();
+			//consume next line character
+			sc.nextLine();
+		}
+		}
+		
+		}
+		
+		// 1 to send message 2. return to menu
+		
 	
-	//get user unique ID
-	private static int getID(String name) {
-			  int id = 0;
-			  for(int i=0;i< name.length(); i++){
-			  	char character = name.charAt(i);
-			    int ascii = (int) character;
-			  	id += ascii;
-			 	 }
-			  return id;
-	}
-	
+	//get user unique ID?
+//	private static int getID(String name) {
+//			  int id = 0;
+//			  for(int i=0;i< name.length(); i++){
+//			  	char character = name.charAt(i);
+//			    int ascii = (int) character;
+//			  	id += ascii;
+//			 	 }
+//			  return id;
+//	}
+//	
 }
 
 
